66609d7ed61a127c013b54a958e8fed6222f4ea3
feluisdev <feluis.dev@gmail.com>
2025-11-04 20:32:08 -0100

    feat(shared): add `TipoAccao` enum and integration with shared constants library
    - Introduced `TipoAccao` enum for standardized action types in the shared module.
    - Generated `TIPOACCAO.json` file for structured representation of action types.
    - Added utility methods for mapping, code-description retrieval, and validation in `TipoAccao` enum.

---
+++ src/main/java/cv/inps/rh/shared/application/constants/TIPOACCAO.java
@@ -0,0 +1,81 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
+package cv.inps.rh.shared.application.constants;
+
+import cv.igrp.framework.core.domain.IgrpEnum;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
+import cv.inps.rh.shared.domain.exceptions.IgrpResponseStatusException;
+
+
+public enum TipoAccao implements IgrpEnum<String> {
+
+  REGISTO_COLABORADOR("REGISTO_COLABORADOR", "REGISTO_COLABORADOR"),
+    CARREIRA("CARREIRA", "CARREIRA"),
+    MOBILIDADE("MOBILIDADE", "MOBILIDADE"),
+    CONTRATO("CONTRATO", "CONTRATO"),
+    RENOVACAO_CONTRATO("RENOVACAO_CONTRATO", "RENOVACAO_CONTRATO"),
+    DADOS_ACADEMICOS("DADOS_ACADEMICOS", "DADOS_ACADEMICOS"),
+    ESTADO_COLABORADOR("ESTADO_COLABORADOR", "ESTADO_COLABORADOR"),
+    DADOS_BANCARIOS("DADOS_BANCARIOS", "DADOS_BANCARIOS"),
+    RENDIMENTO("RENDIMENTO", "RENDIMENTO"),
+    DESCONTO("DESCONTO", "DESCONTO"),
+    SUBSTITUICAO_MOBILIDADE("SUBSTITUICAO_MOBILIDADE", "SUBSTITUICAO_MOBILIDADE")
+  ;
+
+  private final String code;
+  private final String description;
+
+  TipoAccao(String code, String description) {
+    this.code = code;
+    this.description = description;
+  }
+
+  @Override
+  public String getCode() {
+    return code;
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+  * Pre-built maps for fast lookup.
+  */
+  private static final Map<String, TipoAccao> CODE_MAP = Arrays.stream(values())
+          .collect(Collectors.toMap(TipoAccao::getCode, Function.identity()));
+
+  /**
+  * Attempts to find the enum value associated with the given code.
+  * @param code The code to look up
+  * @return An Optional containing the enum value if found, empty Optional otherwise
+  */
+  public static Optional<TipoAccao> fromCode(String code) {
+    return Optional.ofNullable(CODE_MAP.get(code));
+  }
+
+  /**
+  * Finds the enum value associated with the given code or throws an exception if not found.
+  * @param code The code to look up
+  * @return The enum value for the given code
+  * @throws IllegalArgumentException if no enum value exists for the given code
+  */
+  public static TipoAccao fromCodeOrThrow(String code) {
+    return fromCode(code).orElseThrow(() -> IgrpResponseStatusException.of(HttpStatus.BAD_REQUEST, "Invalid TipoAccao for this code: " + code));
+  }
+
+  /**
+  * Returns a map of code to description.
+  */
+  public static Map<String, String> codeDescriptionMap() {
+    return CODE_MAP.values().stream().collect(Collectors.toMap(TipoAccao::getCode, TipoAccao::getDescription));
+  }
+
+}
