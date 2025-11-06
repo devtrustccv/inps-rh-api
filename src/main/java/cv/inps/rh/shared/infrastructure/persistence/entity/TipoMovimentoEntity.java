5f3b43956d096f7069f689d1b614cbf6d6ff917a
feluisdev <feluis.dev@gmail.com>
2025-11-06 20:34:59 -0100

    feat(shared): extend `TipoMovimentoEntity` with audit and new attributes
    - Added auditing support by extending `AuditEntity`.
    - Introduced multiple new fields (e.g., `tipo`, `tipoProcessamento`, `iacIacId`, `tipoSal`, etc.) to enhance the entity's metadata.
    - Updated JSON model to reflect the new structure and properties.

--- src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/TipoMovimentoEntity.java
+++ src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/TipoMovimentoEntity.java
@@ -3,6 +3,7 @@
 
 package cv.inps.rh.shared.infrastructure.persistence.entity;
 
+import cv.inps.rh.shared.config.AuditEntity;
 import cv.igrp.framework.stereotype.IgrpEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -17,7 +18,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "RH_TIPO_MOVIMENTOS")
-public class TipoMovimentoEntity  {
+public class TipoMovimentoEntity extends AuditEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,6 +37,74 @@ public class TipoMovimentoEntity  {
   
     @Column(name="percentagem")
     private BigDecimal percentagem;
+
+  
+    @Column(name="tipo")
+    private String tipo;
+
+  
+    @Column(name="tipo_processamento")
+    private String tipoProcessamento;
+
+  
+    @Column(name="iac_iac_id")
+    private Long iacIacId;
+
+  
+    @Column(name="cobre_imp")
+    private String cobreImp;
+
+  
+    @Column(name="social")
+    private String social;
+
+  
+    @Column(name="calculo")
+    private String calculo;
+
+  
+    @Column(name="favor_estado")
+    private String favorEstado;
+
+  
+    @Column(name="iac_iac_rec_id")
+    private Long iacIacRecId;
+
+  
+    @Column(name="actualiza_conta_corrente")
+    private String actualizaContaCorrente;
+
+  
+    @Column(name="tipo_iur")
+    private String tipoIur;
+
+  
+    @Column(name="amb_apl_id")
+    private Long ambAplId;
+
+  
+    @Column(name="retencao")
+    private String retencao;
+
+  
+    @Column(name="cc_id")
+    private Long ccId;
+
+  
+    @Column(name="ent_id")
+    private String entId;
+
+  
+    @Column(name="tipo_sal")
+    private String tipoSal;
+
+  
+    @Column(name="acumulado")
+    private String acumulado;
+
+  
+    @Column(name="estado")
+    private String estado;
 
   
 }
