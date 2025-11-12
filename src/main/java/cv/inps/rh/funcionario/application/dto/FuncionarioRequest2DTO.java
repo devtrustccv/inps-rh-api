f2dae845f2e5b6c56a43948dedb9f13e9c7512ef
feluisdev <feluis.dev@gmail.com>
2025-11-12 20:59:40 -0100

    feat(funcionario): add DTOs `DadosContratuaisReq2DTO` and `FuncionarioRequest2DTO`
    - Created `DadosContratuaisReq2DTO` and `FuncionarioRequest2DTO` classes to manage contract and employee request data.
    - Added corresponding JSON definitions for integration with iGRP Studio.
    - Enabled validation and defined relationships with related DTOs (`SubsidioReqDTO`, `EncargosDescontosReqDTO`, etc.).

---
+++ src/main/java/cv/inps/rh/funcionario/application/dto/FuncionarioRequest2DTO.java
@@ -0,0 +1,48 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
+package cv.inps.rh.funcionario.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import cv.inps.rh.funcionario.application.dto.AgregadoDependenteReqDTO;
+import cv.inps.rh.funcionario.application.dto.AnexoReqDTO;
+import cv.inps.rh.funcionario.application.dto.DadosAcademicosProfReqDTO;
+import cv.inps.rh.funcionario.application.dto.DadosBancariosReqDTO;
+import cv.inps.rh.funcionario.application.dto.DadosContratuaisReq2DTO;
+import cv.inps.rh.funcionario.application.dto.DadosPessoaisReqDTO;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class FuncionarioRequest2DTO  {
+
+  
+  @Valid
+  private DadosPessoaisReqDTO dadosPessoais ;
+  
+  @Valid
+  private List<AgregadoDependenteReqDTO> familiares = new ArrayList<>();
+  
+  @Valid
+  private DadosAcademicosProfReqDTO dadosAcademicosProf ;
+  
+  @Valid
+  private DadosContratuaisReq2DTO dadosContratuais ;
+  
+  @Valid
+  private List<DadosBancariosReqDTO> dadosBancarios = new ArrayList<>();
+  
+  @Valid
+  private List<AnexoReqDTO> anexos = new ArrayList<>();
+
+}
