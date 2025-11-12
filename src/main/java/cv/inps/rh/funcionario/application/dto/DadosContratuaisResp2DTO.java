7e228bbc0fb87339814f3c9b1d3cdb618d4d01cd
feluisdev <feluis.dev@gmail.com>
2025-11-12 21:03:45 -0100

    feat(funcionario): add DTOs `DadosContratuaisResp2DTO` and `FuncionarioResponse2DTO`
    - Created `DadosContratuaisResp2DTO` and `FuncionarioResponse2DTO` classes for handling contract and employee response data.
    - Added corresponding JSON definitions for integration with iGRP Studio.
    - Established validation and relationships with related DTOs (`SubsidioRespDTO`, `EncargosDescontosRespDTO`, etc.).

---
+++ src/main/java/cv/inps/rh/funcionario/application/dto/DadosContratuaisResp2DTO.java
@@ -0,0 +1,88 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
+package cv.inps.rh.funcionario.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import cv.inps.rh.funcionario.application.dto.EncargosDescontosRespDTO;
+import cv.inps.rh.funcionario.application.dto.SubsidioRespDTO;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class DadosContratuaisResp2DTO  {
+
+  
+  
+  private Long tipoContratoId ;
+  
+  
+  private Long cargoPosicaoId ;
+  
+  
+  private Long direcaoId ;
+  
+  
+  private Long seccaoId ;
+  
+  
+  private String centroCusto ;
+  
+  
+  private Long carreiraId ;
+  
+  
+  private Long categoriaId ;
+  
+  
+  private Long escalaoReferenciaId ;
+  
+  
+  private Long tipoVinculoLaboralId ;
+  
+  
+  private String RegimeTrabalho ;
+  
+  
+  private BigDecimal salario ;
+  
+  
+  private String moeda ;
+  
+  
+  private LocalDate dataInicio ;
+  
+  
+  private LocalDate dataFim ;
+  
+  
+  private Integer duracaoMeses ;
+  
+  
+  private Long localTrabalhoId ;
+  
+  
+  private Long paisId ;
+  
+  
+  private Long ilhaId ;
+  
+  @Valid
+  private List<SubsidioRespDTO> subsidios = new ArrayList<>();
+  
+  @Valid
+  private List<EncargosDescontosRespDTO> encargosDescontos = new ArrayList<>();
+
+}
