881b7df56b649b1c12d875888d8b4dd86d19c3a9
william.fonseca <william.fonseca@nosi.cv>
2025-11-16 15:12:57 -0100

    refactor: switch ParamCargoEntity ID generation strategy to sequence

--- src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/ParamCargoEntity.java
+++ src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/ParamCargoEntity.java
@@ -3,13 +3,17 @@
 
 package cv.inps.rh.shared.infrastructure.persistence.entity;
 
-import cv.inps.rh.shared.config.AuditEntity;
 import cv.igrp.framework.stereotype.IgrpEntity;
-import jakarta.persistence.*;
-import lombok.*;
-import java.util.UUID;
-import jakarta.validation.constraints.NotBlank;
 import cv.inps.rh.shared.application.constants.Estado;
+import cv.inps.rh.shared.config.AuditEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
 
 
 @Getter
@@ -22,20 +26,19 @@ import cv.inps.rh.shared.application.constants.Estado;
 public class ParamCargoEntity extends AuditEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "seq_param_cargo", sequenceName = "SEQ_PARAM_CARGO", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_param_cargo")
     @Column(name = "id", unique = true, nullable = false)
     private Long id;
 
-  
-    @Column(name="uuid")
+
+  @Column(name="uuid")
     private UUID uuid;
 
-  
-    @NotBlank(message = "nome is mandatory")
+
+  @NotBlank(message = "nome is mandatory")
     @Column(name="nome", nullable = false)
     private String nome;
-
-  
 
 
   @ManyToOne(fetch = FetchType.LAZY)
@@ -44,10 +47,10 @@ public class ParamCargoEntity extends AuditEntity {
     @Column(name="dirigente", length=1)
     private String dirigente;
 
-  
-    @Enumerated(EnumType.STRING)
+
+  @Enumerated(EnumType.STRING)
     @Column(name="estado")
     private Estado estado;
 
-  
+
 }
