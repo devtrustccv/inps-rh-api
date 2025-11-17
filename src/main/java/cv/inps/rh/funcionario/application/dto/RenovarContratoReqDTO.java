ff19a65950b40d2d52f7bbafdec8107a4bffe68a
feluisdev <feluis.dev@gmail.com>
2025-11-17 15:40:34 -0100

    feat(funcionario): add RenovarContratoReqDTO and corresponding JSON configuration

---
+++ src/main/java/cv/inps/rh/funcionario/application/dto/RenovarContratoReqDTO.java
@@ -0,0 +1,38 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
+package cv.inps.rh.funcionario.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class RenovarContratoReqDTO  {
+
+  
+  
+  private Long tipoContratoId ;
+  
+  
+  private Long tipoVinculoId ;
+  
+  
+  private LocalDate dataInicio ;
+  
+  
+  private LocalDate dataFim ;
+  
+  
+  private Integer duracaoMeses ;
+
+}
