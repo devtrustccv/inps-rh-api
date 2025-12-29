8663256b9571b074d93b933e050b317e186d9a6b
feluisdev <feluis.dev@gmail.com>
2025-12-29 13:13:31 -0100

    refactor: update `GetHistoricoLaboralById` query handler with service integration and implement full mapping logic in `HistoricoLaboralReadService`

--- src/main/java/cv/inps/rh/funcionario/application/queries/GetHistoricoLaboralByIdQueryHandler.java
+++ src/main/java/cv/inps/rh/funcionario/application/queries/GetHistoricoLaboralByIdQueryHandler.java
@@ -4,20 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import cv.igrp.framework.core.domain.QueryHandler;
 import cv.igrp.framework.stereotype.IgrpQueryHandler;
-import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import cv.inps.rh.funcionario.application.dto.ValidarNovoHistoricoLaboralDTO;
+import cv.inps.rh.funcionario.application.service.historicolaboral.HistoricoLaboralReadService;
 
 @Component
 public class GetHistoricoLaboralByIdQueryHandler implements QueryHandler<GetHistoricoLaboralByIdQuery, ResponseEntity<ValidarNovoHistoricoLaboralDTO>>{
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GetHistoricoLaboralByIdQueryHandler.class);
 
+  private final HistoricoLaboralReadService historicoLaboralReadService;
 
-  public GetHistoricoLaboralByIdQueryHandler() {
-
+  public GetHistoricoLaboralByIdQueryHandler(HistoricoLaboralReadService historicoLaboralReadService) {
+    this.historicoLaboralReadService = historicoLaboralReadService;
   }
 
    @IgrpQueryHandler
@@ -25,8 +26,8 @@ public class GetHistoricoLaboralByIdQueryHandler implements QueryHandler<GetHist
 
     LOGGER.debug("GetHistoricoLaboralByIdQuery: {}", query);
 
-    // TODO: Implement the query handling logic here
-    return null;
+    var data = historicoLaboralReadService.getHistoricoLaboralById(query);
+    return ResponseEntity.ok(data);
   }
 
 }
--- src/main/java/cv/inps/rh/funcionario/application/service/historicolaboral/HistoricoLaboralReadService.java
+++ src/main/java/cv/inps/rh/funcionario/application/service/historicolaboral/HistoricoLaboralReadService.java
@@ -25,79 +25,149 @@ import static java.util.Optional.ofNullable;
 @Transactional
 public class HistoricoLaboralReadService {
 
-  private final TiposRelacionamentoEntityRepository tiposRelacionamentoEntityRepository;
+    private final TiposRelacionamentoEntityRepository tiposRelacionamentoEntityRepository;
 
-  public WrapperHistLaboralResponseDTO getHistoricoLaboral(GetHistoricoLaboralQuery query) {
+    public WrapperHistLaboralResponseDTO getHistoricoLaboral(GetHistoricoLaboralQuery query) {
 
-    var pageRequest = PageRequest.of(
-        Integer.parseInt(query.getPagina()),
-        Integer.parseInt(query.getTamanho())
-    );
+        var pageRequest = PageRequest.of(
+                Integer.parseInt(query.getPagina()),
+                Integer.parseInt(query.getTamanho()));
 
-    var page = tiposRelacionamentoEntityRepository.findByFunId_UuidAndEstado(
-        UUID.fromString(query.getFuncionarioId()),
-        Estado.A,
-        pageRequest
-    );
+        var page = tiposRelacionamentoEntityRepository.findByFunId_UuidAndEstado(
+                UUID.fromString(query.getFuncionarioId()),
+                Estado.A,
+                pageRequest);
 
-    var data = page.stream()
-        .map(obj -> {
-          var response = new HistoricoLaboralResponseDTO();
-          response.setUltimoMovimento(DateFormatter.localDateToString(obj.getUltProc()));
+        var data = page.stream()
+                .map(obj -> {
+                    var response = new HistoricoLaboralResponseDTO();
+                    response.setUltimoMovimento(DateFormatter.localDateToString(obj.getUltProc()));
 
-          ofNullable(obj.getTipoSituacao()).ifPresent(response::setTipoSituacao);
-          ofNullable(obj.getContrVinculoId().getTpContratoId().getNome()).ifPresent(response::setTipoContrato);
-          ofNullable(obj.getContrVinculoId().getVinculoId()).map(ParamVinculoEntity::getNome).ifPresent(response::setVinculo);
-          ofNullable(obj.getSeccaoId()).map(SecaoEntity::getInstId).map(InstituicaoEntity::getNome).ifPresent(response::setDirecao);
-          ofNullable(obj.getSeccaoId()).map(SecaoEntity::getNome).ifPresent(response::setSeccao);
-          ofNullable(obj.getCarreiraId()).map(CarreiraEntity::getCarrPccsId).map(ParamCarreiraEntity::getNome).ifPresent(response::setCarreira);
-          ofNullable(obj.getEscalaoId()).map(ParamEscalaoEntity::getEscalao).ifPresent(response::setReferenciaEscalao);
-          ofNullable(obj.getCargoId()).map(ParamCargoEntity::getNome).ifPresent(response::setCargo);
-          ofNullable(obj.getSituacLaboralId())
-              .map(SituacaoLaboralEntity::getSituacaoLaboralId)
-              .map(ParamSituacaoEntity::getNome)
-              .ifPresent(response::setSituacaoLaboral);
+                    ofNullable(obj.getTipoSituacao()).ifPresent(response::setTipoSituacao);
+                    ofNullable(obj.getContrVinculoId().getTpContratoId().getNome())
+                            .ifPresent(response::setTipoContrato);
+                    ofNullable(obj.getContrVinculoId().getVinculoId()).map(ParamVinculoEntity::getNome)
+                            .ifPresent(response::setVinculo);
+                    ofNullable(obj.getSeccaoId()).map(SecaoEntity::getInstId).map(InstituicaoEntity::getNome)
+                            .ifPresent(response::setDirecao);
+                    ofNullable(obj.getSeccaoId()).map(SecaoEntity::getNome).ifPresent(response::setSeccao);
+                    ofNullable(obj.getCarreiraId()).map(CarreiraEntity::getCarrPccsId).map(ParamCarreiraEntity::getNome)
+                            .ifPresent(response::setCarreira);
+                    ofNullable(obj.getEscalaoId()).map(ParamEscalaoEntity::getEscalao)
+                            .ifPresent(response::setReferenciaEscalao);
+                    ofNullable(obj.getCargoId()).map(ParamCargoEntity::getNome).ifPresent(response::setCargo);
+                    ofNullable(obj.getSituacLaboralId())
+                            .map(SituacaoLaboralEntity::getSituacaoLaboralId)
+                            .map(ParamSituacaoEntity::getNome)
+                            .ifPresent(response::setSituacaoLaboral);
 
-          response.setId(obj.getId());
-          response.setUuid(obj.getFunId().getUuid().toString());
+                    response.setId(obj.getId());
+                    response.setUuid(obj.getFunId().getUuid().toString());
 
-          var dataInicioContrato = obj.getContrVinculoId().getDataInicio()!=null ? DateFormatter.localDateToString(obj.getContrVinculoId().getDataInicio())
-              : StringUtils.EMPTY;
-          var dataFimContrato = obj.getContrVinculoId().getDataFim()!=null ? DateFormatter.localDateToString(obj.getContrVinculoId().getDataFim())
-              : StringUtils.EMPTY;
+                    var dataInicioContrato = obj.getContrVinculoId().getDataInicio() != null
+                            ? DateFormatter.localDateToString(obj.getContrVinculoId().getDataInicio())
+                            : StringUtils.EMPTY;
+                    var dataFimContrato = obj.getContrVinculoId().getDataFim() != null
+                            ? DateFormatter.localDateToString(obj.getContrVinculoId().getDataFim())
+                            : StringUtils.EMPTY;
 
-          var dataInicioCarreira = obj.getCarreiraId()!=null ? DateFormatter.localDateToString(obj.getCarreiraId().getDataInicio())
-              : StringUtils.EMPTY;
-          var dataFimCarreira = obj.getCarreiraId()!=null ? DateFormatter.localDateToString(obj.getCarreiraId().getDataFim())
-              : StringUtils.EMPTY;
+                    var dataInicioCarreira = obj.getCarreiraId() != null
+                            ? DateFormatter.localDateToString(obj.getCarreiraId().getDataInicio())
+                            : StringUtils.EMPTY;
+                    var dataFimCarreira = obj.getCarreiraId() != null
+                            ? DateFormatter.localDateToString(obj.getCarreiraId().getDataFim())
+                            : StringUtils.EMPTY;
 
-          response.setDataInicioContratoCarreira(dataInicioContrato.concat(" / ").concat(dataInicioCarreira));
-          response.setDataFimContratoCarreira(dataFimContrato.concat(" / ").concat(dataFimCarreira));
+                    response.setDataInicioContratoCarreira(dataInicioContrato.concat(" / ").concat(dataInicioCarreira));
+                    response.setDataFimContratoCarreira(dataFimContrato.concat(" / ").concat(dataFimCarreira));
 
-          var start = DateFormatter.localDateToString(obj.getDataInicio());
-          var end = obj.getDataFim() != null ? " / " + DateFormatter.localDateToString(obj.getDataFim()) : StringUtils.EMPTY;
-          response.setDataInicioFim(start + end);
-          return response;
-        }).toList();
+                    var start = DateFormatter.localDateToString(obj.getDataInicio());
+                    var end = obj.getDataFim() != null ? " / " + DateFormatter.localDateToString(obj.getDataFim())
+                            : StringUtils.EMPTY;
+                    response.setDataInicioFim(start + end);
+                    return response;
+                }).toList();
 
-    var wrapper = new WrapperHistLaboralResponseDTO();
-    wrapper.setHistorico(data);
-    wrapper.setPageNumber(page.getTotalPages());
-    wrapper.setPageSize(page.getSize());
-    wrapper.setTotalElements(page.getTotalElements());
-    wrapper.setTotalPages(page.getTotalPages());
-    wrapper.setLast(page.isLast());
-    wrapper.setFirst(page.isFirst());
+        var wrapper = new WrapperHistLaboralResponseDTO();
+        wrapper.setHistorico(data);
+        wrapper.setPageNumber(page.getTotalPages());
+        wrapper.setPageSize(page.getSize());
+        wrapper.setTotalElements(page.getTotalElements());
+        wrapper.setTotalPages(page.getTotalPages());
+        wrapper.setLast(page.isLast());
+        wrapper.setFirst(page.isFirst());
 
-    return wrapper;
-  }
+        return wrapper;
+    }
 
-  public ValidarNovoHistoricoLaboralDTO getHistoricoLaboralById(GetHistoricoLaboralByIdQuery query) {
-    var entity = tiposRelacionamentoEntityRepository.findByUuid(UUID.fromString(query.getHistoricoId()))
-        .orElseThrow(() -> IgrpResponseStatusException.notFound("Histórico Laboral não encontrado"));
+    public ValidarNovoHistoricoLaboralDTO getHistoricoLaboralById(GetHistoricoLaboralByIdQuery query) {
+        var entity = tiposRelacionamentoEntityRepository.findByUuid(UUID.fromString(query.getHistoricoId()))
+                .orElseThrow(() -> IgrpResponseStatusException.notFound("Histórico Laboral não encontrado"));
 
-    return null;
-  }
+        var dto = new ValidarNovoHistoricoLaboralDTO();
 
+        var contrato = entity.getContrVinculoId();
+        if (contrato != null) {
+            dto.setContrato(contrato.getTpContratoId() != null ? contrato.getTpContratoId().getNome() : null);
+            dto.setVinculo(contrato.getVinculoId() != null ? contrato.getVinculoId().getNome() : null);
+        }
+
+        dto.setSalario(entity.getSalario() != null
+                ? entity.getSalario()
+                : (entity.getCarreiraId() != null ? entity.getCarreiraId().getSalario() : null));
+
+        var mob = entity.getMobId();
+        if (mob != null) {
+            dto.setTipoMobilidade(mob.getTipoSituacao());
+            dto.setDataInicioMobilidade(mob.getDataInicio());
+            dto.setDataFimMobilidade(mob.getDataFim());
+        }
+
+        var inst = entity.getInstitId();
+        if (inst != null)
+            dto.setDirecao(inst.getId());
+        var sec = entity.getSeccaoId();
+        if (sec != null)
+            dto.setSecao(sec.getId());
+        var lt = entity.getLocTrabId();
+        if (lt != null) {
+            dto.setLocalTrabalho(lt.getId());
+            dto.setPais(lt.getPaisId() != null ? lt.getPaisId().getNome() : null);
+            dto.setIlha(lt.getIlhaId() != null ? lt.getIlhaId().getId() : null);
+        }
+
+        var car = entity.getCarreiraId();
+        if (car != null) {
+            dto.setTipoAlteracaoCarreira(car.getTipoSituacao());
+            dto.setDataInicioCarreira(car.getDataInicio());
+            dto.setDataFimCarreira(car.getDataFim());
+        }
+        var carrPcc = entity.getCarrPccId();
+        if (carrPcc != null)
+            dto.setCarreira(carrPcc.getId());
+        var cat = entity.getCategoriaId();
+        if (cat != null)
+            dto.setCategoria(cat.getId());
+        var esc = entity.getEscalaoId();
+        if (esc != null)
+            dto.setEscalao(esc.getId());
+        var cargo = entity.getCargoId();
+        if (cargo != null)
+            dto.setCargo(cargo.getId());
+
+        var sit = entity.getSituacLaboralId();
+        if (sit != null) {
+            var paramSit = sit.getSituacaoLaboralId();
+            dto.setSituacaoLaboral(paramSit != null ? paramSit.getId() : null);
+            dto.setMotivo(sit.getMotivoSitLabId() != null ? String.valueOf(sit.getMotivoSitLabId().getId())
+                    : sit.getMotivoSitLab());
+            dto.setDataInicioSituacao(sit.getDataInicio());
+            dto.setDataFimSituacao(sit.getDataFim());
+            dto.setObservacao(sit.getObs());
+            dto.setSituacaoAtual(paramSit != null ? paramSit.getNome() : null);
+        }
+
+        return dto;
+    }
 
 }
