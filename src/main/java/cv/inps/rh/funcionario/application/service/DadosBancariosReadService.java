4b9d11161854c92c127e8506b351bb47b2189e05
feluisdev <feluis.dev@gmail.com>
2026-01-15 19:27:20 -0100

    feat: add filtering of `DadosBancariosEntity` by `FuncionarioId` and states
    - Added `findByFuncionarioIdAndEstados` method in `DadosBancariosEntityRepository`.
    - Updated `DadosBancariosReadService` to use the new method for filtering by active and inactive states.

--- src/main/java/cv/inps/rh/funcionario/application/service/DadosBancariosReadService.java
+++ src/main/java/cv/inps/rh/funcionario/application/service/DadosBancariosReadService.java
@@ -3,7 +3,9 @@ package cv.inps.rh.funcionario.application.service;
 import cv.inps.rh.funcionario.application.dto.DadosBancariosRespDTO;
 import cv.inps.rh.funcionario.application.queries.GetDadosBancariosQuery;
 import cv.inps.rh.funcionario.infrastructure.mappers.DadosBancariosMapper;
+import cv.inps.rh.shared.application.constants.Estado;
 import cv.inps.rh.shared.domain.models.IdentificadorUnico;
+import cv.inps.rh.shared.infrastructure.persistence.repository.DadosBancariosEntityRepository;
 import cv.inps.rh.shared.infrastructure.persistence.repository.FuncionarioEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,11 +18,18 @@ import java.util.List;
 public class DadosBancariosReadService {
 
   private final FuncionarioEntityRepository funcionarioEntityRepository;
+  private final DadosBancariosEntityRepository dadosBancariosEntityRepository;
   private final DadosBancariosMapper dadosBancariosMapper;
 
   @Transactional(readOnly = true)
   public List<DadosBancariosRespDTO> getDadosBancarios(GetDadosBancariosQuery query) {
     var funcionario = funcionarioEntityRepository.findByUuidOrThrow(IdentificadorUnico.from(query.getIdFuncionario()).valor());
-    return dadosBancariosMapper.toDadosBancariosRespDTOList(funcionario.getDadosBancarios());
+
+    var estados = List.of(Estado.A, Estado.I);
+
+    var dadosBancarios  = dadosBancariosEntityRepository.findByFuncionarioIdAndEstados(
+        funcionario.getUuid(), estados
+    );
+    return dadosBancariosMapper.toDadosBancariosRespDTOList(dadosBancarios);
   }
 }
