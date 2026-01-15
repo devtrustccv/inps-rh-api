c5cb8028c854bcd6962753402b2a0e513dafebd1
feluisdev <feluis.dev@gmail.com>
2026-01-15 19:36:00 -0100

    refactor: remove redundant blank line in `RegistarColaboradorService`

--- src/main/java/cv/inps/rh/funcionario/application/service/RegistarColaboradorService.java
+++ src/main/java/cv/inps/rh/funcionario/application/service/RegistarColaboradorService.java
@@ -233,7 +233,6 @@ public class RegistarColaboradorService {
     tr.setFlgProcessa(0);
     tr.setEstActAdm(1);
     tr.setSituacLaboralId(situacaoLaboral);
-
     fun.setTiposrelacionamentos(new ArrayList<>(List.of(tr)));
 
     var valid = dadosContratuaisMapper.toValidacaoInsert(TipoAcao.INSERT.name(), Referencia.REGISTO_COLABORADOR.name(),
