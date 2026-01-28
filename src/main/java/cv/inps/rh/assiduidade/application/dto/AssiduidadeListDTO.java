9e45cc50169df6e82c5f2cda1fecd405434e5678
feluisdev <feluis.dev@gmail.com>
2026-01-28 20:02:52 -0100

    refactor: simplify `MovimentoResumoService` by removing unused methods and improving date parsing logic
    - Removed redundant methods including aggregators and subqueries in `MovimentoResumoService`.
    - Streamlined `DateFormatter` usage for `dataInicio` and `dataFim` parsing.
    - Cleaned up `AssiduidadeListDTO` by removing extra whitespace and fixing structure.

--- src/main/java/cv/inps/rh/assiduidade/application/dto/AssiduidadeListDTO.java
+++ src/main/java/cv/inps/rh/assiduidade/application/dto/AssiduidadeListDTO.java
@@ -19,44 +19,45 @@ import java.util.UUID;
 @IgrpDTO
 public class AssiduidadeListDTO  {
 
-  
-  
+
+
   private Long id ;
-  
-  
+
+
   private UUID uuid ;
-  
-  
+
+
   private UUID uuidFuncionairio ;
-  
-  
+
+
   private String nomeColaborador ;
-  
-  
+
+
   private String direcao ;
-  
-  
+
+
   private Integer totalFalta ;
-  
-  
+
+
   private Integer totalDias ;
-  
-  
+
+
   private Integer totalHorasTrabalhadas ;
-  
-  
+
+
   private Integer totalHorasAusentes ;
-  
-  
+
+
   private Integer totalHoraExtra ;
-  
-  
+
+
   private Integer totalHoraAlmoco ;
-  
-  
+
+
   private String estado ;
-  
-  
+
+
   private String estadoDesc ;
+
 
 }
