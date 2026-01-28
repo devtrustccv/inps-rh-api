617cabc6d851dc60948c736c3e60aaaf7caaf708
feluisdev <feluis.dev@gmail.com>
2026-01-28 11:11:51 -0100

    feat: add `AusenciaEntity` model and repository
    - Introduced `AusenciaEntity` model with fields like `id`, `param_sit_id`, `referencia_name`, `uuid`, `data_inicio`, and `estado`.
    - Created `AusenciaEntityRepository` with a custom `findByIdOrThrow` method for enhanced exception handling.
    - Synchronized changes in JSON model to map the entity and table structure.

---
+++ src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/AusenciaEntity.java
@@ -0,0 +1,71 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.inps.rh.shared.infrastructure.persistence.entity;
+
+import cv.inps.rh.shared.config.AuditEntity;
+import cv.igrp.framework.stereotype.IgrpEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import java.time.LocalDate;
+import cv.inps.rh.shared.application.constants.Estado;
+
+
+@Getter
+@Setter
+@IgrpEntity
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "RH_T_AUSENCIA")
+public class AusenciaEntity extends AuditEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_ausencia")
+    @SequenceGenerator(name = "seq_ausencia", sequenceName = "SEQ_AUSENCIA", allocationSize = 1)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+  
+    @NotNull(message = "paramSitId is mandatory")
+
+
+  @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "param_sit_id", referencedColumnName = "id")
+    private ParamSituacaoEntity paramSitId;
+    @Column(name="referencia_name")
+    private String referenciaName;
+
+  
+    @Column(name="referencia_id")
+    private Long referenciaId;
+
+  
+    @Column(name="obs")
+    private String obs;
+
+  
+    @Column(name="uuid")
+    private UUID uuid;
+
+  
+    @Column(name="hora")
+    private Integer hora;
+
+  
+    @Column(name="data_inicio")
+    private LocalDate dataInicio;
+
+  
+    @Column(name="data_fim")
+    private LocalDate dataFim;
+
+  
+    @Enumerated(EnumType.STRING)
+    @Column(name="estado")
+    private Estado estado;
+
+  
+}
