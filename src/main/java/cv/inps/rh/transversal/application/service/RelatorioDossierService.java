f8a8ee89bcf9077ab3148b72f615358ce062bcb2
feluisdev <feluis.dev@gmail.com>
2026-02-20 10:05:14 -0100

    feat: add `get` method to `RelatorioDossierService`
    - Introduced `get(RelatorioDossierColaboradorQuery)` to handle dossier collaborator queries.
    - Used `DossierColaboradorListDTO` as the response type.

--- src/main/java/cv/inps/rh/transversal/application/service/RelatorioDossierService.java
+++ src/main/java/cv/inps/rh/transversal/application/service/RelatorioDossierService.java
@@ -1,6 +1,8 @@
 package cv.inps.rh.transversal.application.service;
 
 import cv.inps.rh.shared.infrastructure.persistence.repository.TiposRelacionamentoEntityRepository;
+import cv.inps.rh.transversal.application.dto.DossierColaboradorListDTO;
+import cv.inps.rh.transversal.application.queries.RelatorioDossierColaboradorQuery;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +15,8 @@ public class RelatorioDossierService {
     this.tiposRelacionamentoEntityRepository = tiposRelacionamentoEntityRepository;
   }
 
+  public DossierColaboradorListDTO get(RelatorioDossierColaboradorQuery query){
+    return null;
+  }
 
 }
