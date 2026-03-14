b3b897ab6d2724fe64f755a3af99346c7b15ec2c
feluisdev <feluis.dev@gmail.com>
2026-03-14 10:28:51 -0100

    refactor: replace `Estado` enum with `String` type in `MissaoColaboradorEntity` and `MissaoServicoEntity`
    - Updated `estado` fields in `MissaoColaboradorEntity` and `MissaoServicoEntity` to use `String` type instead of `Estado` enum.
    - Adjusted model metadata and removed `Estado` enum references.

--- src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/MissaoColaboradorEntity.java
+++ src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/MissaoColaboradorEntity.java
@@ -1,12 +1,9 @@
 package cv.inps.rh.shared.infrastructure.persistence.entity;
 
 import cv.igrp.framework.stereotype.IgrpEntity;
-import cv.inps.rh.shared.application.constants.Estado;
 import cv.inps.rh.shared.config.AuditEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -48,9 +45,8 @@ public class MissaoColaboradorEntity extends AuditEntity {
     @JoinColumn(name = "missao_serv_id", referencedColumnName = "id")
     private MissaoServicoEntity missaoServId;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "estado", length = 1)
-    private Estado estado;
+    private String estado;
 
     @Column(name = "uuid", length = 100)
     private UUID uuid;
