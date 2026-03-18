4360e78c9b1a5b5dafe09180292fc57e2751eb7f
feluisdev <feluis.dev@gmail.com>
2026-03-18 20:20:23 -0100

    feat: generate DTOs for mission request emission module
    - Added `MissaoEmissaoRequisicaoRequestDTO` and `MissaoRequisicaoItemRequestDTO` JSON metadata files and corresponding Java classes.
    - Integrated auto-generated DTOs with validation annotations and consistent structure.

---
+++ src/main/java/cv/inps/rh/missaoservico/application/dto/MissaoEmissaoRequisicaoRequestDTO.java
@@ -0,0 +1,27 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.inps.rh.missaoservico.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class MissaoEmissaoRequisicaoRequestDTO  {
+
+  private List<MissaoRequisicaoItemRequestDTO> requisicoes;
+
+
+}
