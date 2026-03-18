6c3751cc58611362042e64018622c27b3b84f55e
feluisdev <feluis.dev@gmail.com>
2026-03-18 18:21:52 -0100

    feat: add `MissaoServicoService` class with basic structure
    - Introduced service annotated with `@Service` and `@RequiredArgsConstructor` for dependency injection setup.

---
+++ src/main/java/cv/inps/rh/missaoservico/application/services/MissaoServicoService.java
@@ -0,0 +1,9 @@
+package cv.inps.rh.missaoservico.application.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MissaoServicoService {
+}
