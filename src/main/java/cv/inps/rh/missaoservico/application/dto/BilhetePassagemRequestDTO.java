067fe277aff50687a0f486c0a1b5d6fdf819c1ac
feluisdev <feluis.dev@gmail.com>
2026-03-18 20:38:14 -0100

    feat: generate DTOs for mission logistics module
    - Added `AjudaCustoRequestDTO`, `AlojamentoRequestDTO`, `BilhetePassagemRequestDTO`, `SeguroViagemRequestDTO`, and `MissaoLogisticaRequestDTO` Java classes and corresponding JSON metadata files.
    - Integrated auto-generated DTOs with validation annotations and consistent structure across mission logistics operations.

---
+++ src/main/java/cv/inps/rh/missaoservico/application/dto/BilhetePassagemRequestDTO.java
@@ -0,0 +1,32 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.inps.rh.missaoservico.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import cv.inps.rh.shared.application.dto.AnexoReqDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class BilhetePassagemRequestDTO  {
+
+  private List<UUID> colaboradorIds;      // multiselect — MissaoColaboradorEntity.id — obrigatório
+  private BigDecimal valor;               // obrigatório
+  private AnexoReqDTO anexo;  // upload PDF — opcional
+
+
+}
