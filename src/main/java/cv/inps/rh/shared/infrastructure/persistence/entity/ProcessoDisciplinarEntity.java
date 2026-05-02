7bd11cedff2e54d0366bfece10021c83406b3c5b
workfegaw <workfegaw@gmail.com>
2026-05-02 17:23:27 -0100

    chore: remove unused fields and update formatting in ProcessoDisciplinarDTO and related classes

--- src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/ProcessoDisciplinarEntity.java
+++ src/main/java/cv/inps/rh/shared/infrastructure/persistence/entity/ProcessoDisciplinarEntity.java
@@ -15,7 +15,6 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.UUID;
 
-
 @Getter
 @Setter
 @IgrpEntity
@@ -25,89 +24,68 @@ import java.util.UUID;
 @Table(name = "RH_T_PROCESSO_DISCIPLINAR")
 public class ProcessoDisciplinarEntity extends AuditEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, nullable = false)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, nullable = false)
+  private Long id;
 
-
-    @NotNull(message = "tiprelId is mandatory")
-
+  @NotNull(message = "tiprelId is mandatory")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "tiprel_id", referencedColumnName = "id")
+  private TiposRelacionamentoEntity tiprelId;
 
   @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "tiprel_id", referencedColumnName = "id")
-    private TiposRelacionamentoEntity tiprelId;
+  @JoinColumn(name = "fun_id", referencedColumnName = "id")
+  private FuncionarioEntity funId;
 
+  @Column(name = "num_proceso")
+  private String numProceso;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "fun_id", referencedColumnName = "id")
-    private FuncionarioEntity funId;
-    @Column(name="num_proceso")
-    private String numProceso;
+  @Column(name = "entidade")
+  private String entidade;
 
+  @Column(name = "tp_processo")
+  private String tpProcesso;
 
-    @Column(name="entidade")
-    private String entidade;
+  @Column(name = "estado")
+  private String estado;
 
+  @Column(name = "pena_discp")
+  private String penaDiscp;
 
-    @Column(name="tp_processo")
-    private String tpProcesso;
+  @Column(name = "date_inic_pd")
+  private LocalDate dateInicPd;
 
+  @Column(name = "date_fim_pd")
+  private LocalDate dateFimPd;
 
-    @Column(name="estado")
-    private String estado;
+  @Column(name = "date_inic_pena")
+  private LocalDate dateInicPena;
 
+  @Column(name = "date_fim_pena")
+  private LocalDate dateFimPena;
 
-    @Column(name="pena_discp")
-    private String penaDiscp;
+  @Column(name = "num_bo")
+  private String numBo;
 
+  @Column(name = "data_publ_bo")
+  private LocalDate dataPublBo;
 
-    @Column(name="date_inic_pd")
-    private LocalDate dateInicPd;
+  @Column(name = "num_ordem_serv")
+  private String numOrdemServ;
 
+  @Column(name = "data_ordem_serv")
+  private LocalDate dataOrdemServ;
 
-    @Column(name="date_fim_pd")
-    private LocalDate dateFimPd;
+  @Column(name = "num_ofa")
+  private String numOfa;
 
+  @Column(name = "data_emiss_ofa")
+  private LocalDate dataEmissOfa;
 
-    @Column(name="date_inic_pena")
-    private LocalDate dateInicPena;
+  @Column(name = "obs", length = 4000)
+  private String obs;
 
-
-    @Column(name="date_fim_pena")
-    private LocalDate dateFimPena;
-
-
-    @Column(name="num_bo")
-    private String numBo;
-
-
-    @Column(name="data_publ_bo")
-    private LocalDate dataPublBo;
-
-
-    @Column(name="num_ordem_serv")
-    private String numOrdemServ;
-
-
-    @Column(name="data_ordem_serv")
-    private LocalDate dataOrdemServ;
-
-
-    @Column(name="num_ofa")
-    private String numOfa;
-
-
-    @Column(name="data_emiss_ofa")
-    private LocalDate dataEmissOfa;
-
-
-    @Column(name="obs", length=4000)
-    private String obs;
-
-
-    @Column(name="uuid")
-    private UUID uuid;
-
-
+  @Column(name = "uuid")
+  private UUID uuid;
 }
