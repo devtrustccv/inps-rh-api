d2e7e22cc83e812cf2b3f148ff5c24049a0fe1da
DevZyon <hericson.cardoso@nosi.cv>
2026-05-03 21:11:55 -0100

    Melhorias DOC (Lista Validacao e Parametro Local)

--- src/main/java/cv/inps/rh/parametrizacao/application/queries/GetLocalTrabalhoAtivosQuery.java
+++ src/main/java/cv/inps/rh/parametrizacao/application/queries/GetLocalTrabalhoAtivosQuery.java
@@ -3,9 +3,17 @@ package cv.inps.rh.parametrizacao.application.queries;
 import cv.igrp.framework.core.domain.Query;
 import jakarta.validation.constraints.*;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetLocalTrabalhoAtivosQuery implements Query {
 
+  @NotNull(message = "The field <paisId> is required")
+  private Long paisId;
 
 }
