542349a75542ccc88701921593de26f1103c2d66
workfegaw <workfegaw@gmail.com>
2026-08-08 13:17:40 -0100

    Fix sorting of configuration list

--- src/main/java/cv/inps/rh/configuracao/application/services/ConfiguracaoGeralService.java
+++ src/main/java/cv/inps/rh/configuracao/application/services/ConfiguracaoGeralService.java
@@ -14,6 +14,7 @@ import cv.inps.rh.shared.infrastructure.persistence.repository.FusoHorarioUpsEnt
 import cv.inps.rh.shared.util.PageMapper;
 import jakarta.validation.Validator;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -110,7 +111,9 @@ public class ConfiguracaoGeralService extends ConfigurationProcess<ConfiguracaoG
 
   @Override
   public Object list(Map<String, String> filters) {
-    var pageable = ConfigurationUtils.buildDefaultPageRequest(filters);
+
+    var pageable = ConfigurationUtils.buildDefaultPageRequest(filters)
+        .withSort(Sort.by(Sort.Direction.DESC, "dtRegisto"));
     var data = repository.findAll(pageable);
 
     var response = new WrapperListDTO();
@@ -162,11 +165,7 @@ public class ConfiguracaoGeralService extends ConfigurationProcess<ConfiguracaoG
     var fusos = new ArrayList<FusoHorarioDTO>();
 
     fusoHorarioUpsEntityRepository.findByIdParametrizacao(e.getId())
-        .forEach(obj -> {
-              var row = new FusoHorarioDTO(obj.getIdUps(), obj.getFuso());
-              fusos.add(row);
-            }
-        );
+        .forEach(obj -> fusos.add(new FusoHorarioDTO(obj.getIdUps(), obj.getFuso())));
 
     r.setFusoHorario(fusos);
 
