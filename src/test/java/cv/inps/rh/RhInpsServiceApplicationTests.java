8581ddfe9bc33bf4f32095abc88ff654ed1aae7c
workfegaw <workfegaw@gmail.com>
2026-08-10 22:20:35 -0100

    Delete tests

--- src/test/java/cv/inps/rh/RhInpsServiceApplicationTests.java
+++ src/test/java/cv/inps/rh/RhInpsServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class RHINPSSERVICEApplicationTests {
+public class RhInpsServiceApplicationTests {
 
   @Test
   public void contextLoads() {
